f329a2df6d49a79da56b546cf41c4262780d8fcf
Ravi Chauhan <ravicha@cybage.com>
2021-06-25 14:17:41 +0530

    first commit

---
+++ Finance/Finance/Behavior/ListViewBehavior.cs
@@ -0,0 +1,34 @@
+﻿using Finance.Model;
+using Finance.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Finance.Behavior
+{
+   public class ListViewBehavior:Behavior<ListView>
+    {
+        ListView listView;
+
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+            listView = bindable;
+            listView.ItemSelected += ListView_ItemSelected;
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            Item selectedItem = (listView.SelectedItem) as Item;
+            Application.Current.MainPage.Navigation.PushAsync(new PostPage(selectedItem));
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            listView.ItemSelected -= ListView_ItemSelected;
+        }
+    }
+}
---
+++ Finance/Finance/ViewModel/MainVM1.cs
@@ -0,0 +1,52 @@
+﻿using Finance.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Finance.ViewModel
+{
+    public class MainVM1 : INotifyPropertyChanged
+    {
+        public Posts Blog
+        {
+            get;
+            set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainVM1()
+        {
+            ReadRss();
+        }
+
+        private void ReadRss()
+        {
+            try {
+                XmlSerializer serializer = new XmlSerializer(typeof(Posts));
+                using (WebClient client = new WebClient())
+                {
+                    string xml = Encoding.Default.GetString(client.DownloadData("https://www.nasa.gov/rss/dyn/shuttle_station.rss"));
+                    using (Stream reader = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
+                    {
+                        Blog = (Posts)serializer.Deserialize(reader);
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
